74d7cc2a00ed0aee4ae4c6d382654b641d349498
Sankalp Mukim <sankalpmukim@gmail.com>
2023-03-23 19:17:24 +0530

    use human detection, upload to firebase

--- main.py
+++ main.py
@@ -1,40 +1,48 @@
-from fastapi import FastAPI
-from detect_people.detect_people import DetectPeople
+import os
+from detect_people.detect_people import HumanDetector
 from dotenv import load_dotenv
-from requests import post
-from os import getenv
+from time import sleep
+from firebase_admin import firestore, initialize_app
+
+# CONSTANTS
+NODES_COLLECTION = "nodes"
+LOGS_COLLECTION = "logs"
+NODE_ID = "0" # os.getenv("NODE_ID")
+WAIT_TIME = 20
 
 load_dotenv()
+app = initialize_app()
+db = firestore.client()
 
-app = FastAPI()
+node_doc_ref = db.collection(NODES_COLLECTION).document(NODE_ID)
+node_doc = node_doc_ref.get()
+node_logs_ref = node_doc_ref.collection(LOGS_COLLECTION)
 
+node_info = node_doc.to_dict()
 
-@app.on_event("startup")
-def startup():
-    try:
-        post(f"{getenv('backend')}/awake", json={"id": getenv('id')})
-    except:
-        pass
-    global dp
-    dp = DetectPeople()
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def get_node_info():
+    node_doc = node_doc_ref.get()
+    node_data = node_doc.to_dict()
+    return node_data
 
 
-@app.get("/detect_people")
-def detect_people():
-    return {"detect_people": dp.detect_people()}
+# endless script to detect people and upload results
+# as logs to firebase
+# keep doing this every WAIT_TIME seconds
+# stop if get_node_info().get("awake") is False
+while True:
+    node_data = get_node_info()
+    if not node_data.get("awake"):
+        print("node is asleep")
+        break
 
+    detector = HumanDetector(write_video=True, display_box=True)
+    detected = detector.detect_people_timedelta()
+    node_logs_ref.add({"people":detected
+                       , "created_at":firestore.SERVER_TIMESTAMP}) # type: ignore
+    print("done")
+    sleep(WAIT_TIME)
 
-@app.on_event("shutdown")
-def shutdown():
-    try:
-        global dp
-        del dp
-        post(f"{getenv('backend')}/awake/dead", json={"id": getenv('id')})
-    except:
-        print("Error")
-        pass
+print("exiting")
+
+# TODO: Use on_snapshot for awake value
